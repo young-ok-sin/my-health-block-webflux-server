c59bf01fcc5b499eb108a2821737b6d814aeaeae
glass <silver20190204@gmail.com>
2024-05-02 04:02:18 +0900

    add project's initial setting

---
+++ src/main/java/com/example/myhealthblock/MyhealthblockApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhealthblock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhealthblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhealthblockApplication.class, args);
+	}
+
+}
