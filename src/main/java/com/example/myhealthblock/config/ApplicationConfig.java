ef48cacc94a9111bd42ca10ef9e0debd7b4cde57
glass <silver20190204@gmail.com>
2024-05-06 00:24:22 +0900

    Add bean definition for our outport interface
    - outport 인터페이스에 대한 적절한 adapter를 지정할 수 있도록 bean 정의 메소드 작성

---
+++ src/main/java/com/example/myhealthblock/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.example.myhealthblock.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class ApplicationConfig {
+
+//    @Bean
+//    public Interface bean() {
+//        return new Impl();
+//    }
+}
