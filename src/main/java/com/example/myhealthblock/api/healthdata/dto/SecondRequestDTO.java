2e617c8ec2ba4e88ff506a22424d3250c202051c
glass <silver20190204@gmail.com>
2024-05-07 05:21:43 +0900

    Fix api request dto errors with no args constructor

--- src/main/java/com/example/myhealthblock/api/healthdata/dto/SecondRequestDTO.java
+++ src/main/java/com/example/myhealthblock/api/healthdata/dto/SecondRequestDTO.java
@@ -2,11 +2,13 @@ package com.example.myhealthblock.api.healthdata.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.HashMap;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class SecondRequestDTO {
